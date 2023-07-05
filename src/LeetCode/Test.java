21e811aec4a9d11a086ca8861eb630a4360dc14e
bablupeddi2ax <79495386+bablupeddi2ax@users.noreply.github.com>
2023-07-05 19:14:08 +0530

    new learnings

--- src/LeetCode/Test.java
+++ src/LeetCode/Test.java
@@ -5,7 +5,39 @@ public class Test {
 	public static void main(String[] args) {
 		int[] nums = {3,2,1,5,6,4};
 		//1,2,3,4.5,6
-		System.out.println(Sol.findKthLargest(nums, 4));
+		//System.out.println(Sol.findKthLargest(nums, 4));
+		int start = 0;
+		int end = 0;
+		int[] arr = {1,2,3,0,1,2,3,0};
+//		for(int i = 0;i<arr.length;i++) {
+//			if(arr[i]==0 && arr[i+1]!=0) {
+//				start=i;
+//				int k = i;
+//				while(arr[k]>0) {
+//					k++;
+//				}
+//				end = k;
+//				
+//			}
+//		}
+		StringBuilder sb= new StringBuilder("101010");
+		Stack<Character> s = new Stack<>();
+		s.push('h');
+	s.pe
+		System.out.println(s.contains('h'));
+		System.out.println(Math.pow(2, 500));
+		double i;
+		for(int j = 0;j<arr.length-1;j++) {
+			i = (Math.random()-0.5);
+		if(i>0) {
+			arr[j] = arr[j]^arr[j+1];
+			arr[j+1] = arr[j]^arr[j+1];
+			arr[j] = arr[j]^arr[j+1];
+		}
+		}
+		for(int j = 0;j<arr.length-1;j++) {
+		System.out.println(arr[j]);
+		}
 	}
 
 }
